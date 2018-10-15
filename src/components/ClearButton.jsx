8e3da00df2486c4cd96c7a0fa3297c5c4fe93b32
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-15 14:10:04 +0200

    clear button

---
+++ src/components/ClearButton.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import 'ClearButton.css';
+
+export const ClearButton = (props) => (
+  <div onClick={props.handleClear} className={'clear-btn'}>
+      {props.children}
+  </div>
+);
