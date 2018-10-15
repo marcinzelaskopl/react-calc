05b14f07b5530eda3d9c0ba0eff12d0571dd3433
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-15 14:34:08 +0200

    more styling for buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './Button.css';
 
 export const Button = props =>
-    <div className={`button-wrapper ${isOperator(props.children) ? null : 'operator'}`}>
+    <div onClick={() => props.handleClick(props.children)} className={`button-wrapper ${isOperator(props.children) ? null : 'operator'}`}>
         {props.children}
     </div>;
 
