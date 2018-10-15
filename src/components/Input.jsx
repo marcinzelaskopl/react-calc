866538c70343991f584d1a258e40bc1b9ab9e51b
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-15 14:05:10 +0200

    app constructor

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Input.css';
 
-export const Input  = () => (
+export const Input  = props => (
    <div className="input">
        {props.input}
    </div>
